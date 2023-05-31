666443abc2bb3a19013a67295a865c804a176ba1
Ursula Cubilla <ursulacubilla@gmail.com>
2023-05-31 14:19:20 +0200

    primer commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Application } from './application';
+import { Welcome } from './welcome';
+import './app.css';
+import { Route, Routes } from 'react-router-dom';
+import { NavBar } from './nav-bar';
+import { Home } from './home';
+import { Login } from './login';
+import { Register } from './register';
+import { Starship } from './startship';
+
+
+function App() {
+  const [efect, setEfect] = useState();
+
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <NavBar />
+        <Routes>
+
+         <Route path='welcome' element={<Welcome /> } />  
+         <Route path='home' element={<Home /> } />  
+         <Route path='application' element={<Application /> } />        
+         <Route path='login' element={<Login /> } />        
+         <Route path='register' element={<Register /> } />
+         <Route path='starships/:starshipId' element={<Starship />} />       
+          </Routes>        
+      
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
